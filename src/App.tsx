c2c5b154fcfafaa6e4c4bd4cd445cf649b857c12
Victor Mora <vmora@peopleready.com>
2023-06-06 16:54:56 -0500

    Add react-query dev tools

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import './App.css';
+import TodoList from './react-query/TodoList';
 
 function App() {
-  return <h1>React Starter Project</h1>;
+  return (
+  <>
+  <h1>Victor starter project</h1>
+  <TodoList/>
+  </>
+  )
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App';
 import './index.css';
 
@@ -11,7 +12,8 @@ ReactDOM.createRoot(
 ).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-    <App />
+      <ReactQueryDevtools/>
+      <App />
     </QueryClientProvider>
   </React.StrictMode>
 );
